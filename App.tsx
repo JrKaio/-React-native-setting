ff647f2d807ca91a365e8ffb2cfd34a7910d9fbe
Kaio Júnior <100144310+JrKaio@users.noreply.github.com>
2022-09-14 16:25:37 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi Kaio</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff223445',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
